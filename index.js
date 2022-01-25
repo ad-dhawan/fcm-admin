d87d2a03bdb4df9e84bc5e590f23560132082c68
Ankit Dhawan <ankit.ad.dhawan@gmail.com>
2022-01-25 20:09:48 +0530

    Firebase Credentials

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const admin = require("firebase-admin");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-const port = process.env.PORT || 3000
-
 //ROUTE IMPORTS
 const notificationRoute = require("./src/routes/notification");
 
 //CONFIGURATION
 dotenv.config();
-const serviceAccount = require("./firebase.json");
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    "type": process.env.FIREBASE_TYPE,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT
+  }),
 });
 
 //DB CONNECT
@@ -31,4 +40,6 @@ app.use(express.json());
 //Route Middleware
 app.use("/notification", notificationRoute);
 
-app.listen(port, () => console.log(`Server is running on ${port}`));
+app.listen(process.env.PORT || 3000, function(){
+  console.log(`Server listening on port ${this.address().port} in ${app.settings.env} mode`);
+});
