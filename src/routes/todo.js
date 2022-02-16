97c8b38f32c2cde74c3c96a575d68808f3554f52
Ankit Dhawan <ankit.ad.dhawan@gmail.com>
2022-02-16 14:24:14 +0530

    [ADDED] get, update

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const TodoSchema = require('../model/todo');
 
-//REGISTER USER
-router.post("/add_todo", async (req, res) => {
+/** CREATE TODO*/
+router.post("/create", async (req, res) => {
     //CREATE NEW TODO
     const todo = new TodoSchema({
         todo: req.body.todo,
@@ -14,8 +14,31 @@ router.post("/add_todo", async (req, res) => {
         const savedTodo = await todo.save();
         res.status(200).json({message: "Todo added", todo: savedTodo});
     } catch(err) { 
-        res.status(500).json({ status: 500, message: "Internal Server Error", error: err });
+        res.status(500).json({ status: 500, message: "Internal Server Error", error: err.toString() });
     }
 });
+
+/** GET ALL TODO SETTINGS */
+router.get('/get_all', (req, res) => {
+    try{
+        TodoSchema.find({}, function(err, todo){
+            if(err) res.status(502).json({error: err});
+            else res.status(200).json(todo)
+        })
+    } catch(err){
+        res.status(500).json({ status: 500, message: "Internal Server Error", error: err.toString() });
+    }
+})
+
+/** UPDATE TODO */
+router.put('/update/:id', async (req, res) => {
+    try{
+        const todo = await TodoSchema.findOne({ _id: req.params.id })
+        await todo.updateOne({ $set : { done: !todo.done } })
+        todo.done ? res.status(200).json({message: `unchecked`, todo: todo}) : res.status(200).json({message: `checked`, todo: todo})
+    } catch(err) {
+        res.status(500).json({ status: 500, message: "Internal Server Error", error: err.toString() });
+    }
+})
 
 module.exports = router;
